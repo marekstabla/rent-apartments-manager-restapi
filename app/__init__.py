d0c4d363e9b5a539a18a9240a80e4e076d52e7ca
Marek Stabla <stabla.m@gmail.com>
2016-01-20 19:53:03 +0100

    Heroku logging

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,12 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.restful import Api
 from flask.ext.cors import CORS
+import sys
+import logging
 
 app = Flask(__name__)
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 CORS(app)
 api = Api(app)
 app.config.from_object('config')
