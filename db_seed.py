b67ff310b7caf14c8045d039ca3652a467dd6934
Marek Stabla <stabla.m@gmail.com>
2016-02-01 23:16:45 +0100

    Added seed improvements and small fixes

--- db_seed.py
+++ db_seed.py
@@ -2,31 +2,34 @@
 from app import db, models
 from sqlalchemy import MetaData
 import time
+import names
 
 # Create users
-number_of_rooms = 6
+number_of_apartments = 3
+number_of_rooms_in_apartment = 2
 users_in_room = 2
 from random import randint
 from random import choice
 
 users = models.User.query.all()
 if len(users) == 0:
-
-    a = models.Apartment(location='Some location')
-    db.session.add(a)
-    db.session.commit()
-    a = models.Apartment.query.get(1)
-    for i in range(0, number_of_rooms):
-        r = models.Room(name='Room'+str(i), apartment_id=a.id,price=200)
-        db.session.add(r)
+    for i in range (0, number_of_apartments):
+        a = models.Apartment(location='Some location ' + str(i))
+        db.session.add(a)
         db.session.commit()
-        r = models.Room.query.get(i+1)
+        a = models.Apartment.query.get(i+1)
+        for ir in range(0, number_of_rooms_in_apartment):
+            r = models.Room(name='Room'+str(ir), apartment_id=a.id,price=200)
+            db.session.add(r)
+            db.session.commit()
+            r = models.Room.query.get((i * number_of_rooms_in_apartment)+ir+1)
 
-        for uir in range(0, users_in_room):
-            u = models.User(firstName='First' + str(uir)+str(i), lastName='Last'+str(uir)+str(i), email='email'+str(uir)+str(i)+'@email.com', room_id=r.id)
-            db.session.add(u)
-        db.session.commit()
+            for uir in range(0, users_in_room):
+                u = models.User(firstName=names.get_first_name(), lastName=names.get_last_name(), email='email'+str(i)+str(uir)+str(ir)+'@email.com', room_id=r.id)
+                db.session.add(u)
+            db.session.commit()
 
+apartments = models.Apartment.query.all()
 bill_types = models.BillType.query.all()
 
 if (len(bill_types) == 0):
@@ -42,20 +45,28 @@ if (len(bill_types) == 0):
     bill_types = models.BillType.query.all()
 
 if models.RentCalculation.query.count() == 0:
-    rc = models.RentCalculation(apartment_id=1, notes='Styczen 2015')
-    bills = 0
+    for ai in range(0, len(apartments)):
+        apartment = models.Apartment.query.get(ai+1)
 
-    for i in range(0, len(bill_types)):
-        b = models.Bill(bill_type_id=i+1, rent_calculation_id=1, amount=randint(100,300), paid=choice([True, False]))
-        db.session.add(b)
-        bills += b.amount
+        for ri in range (0, 10):
+            rc = models.RentCalculation(apartment_id=apartment.id, notes=str(ri+1)+'/2015')
+            db.session.add(rc)
+            db.session.commit()
 
-    db.session.commit()
+            rc = models.RentCalculation.query.get((ai * 10)+ri+1)
+            bills = 0
 
-    for i in range(0, len(users)):
-        u = models.User.query.get(i+1)
-        c = models.Charge(bills=bills, rent=u.room.price, paid=choice([True, False]), rent_calculation_id=1)
-        db.session.add(c)
+            for i in range(0, len(bill_types)):
+                b = models.Bill(bill_type_id=i+1, rent_calculation_id=rc.id, amount=randint(100,300), paid=choice([True, False]))
+                db.session.add(b)
+                bills += b.amount
 
-    db.session.commit()
+            db.session.commit()
+
+            for i in range(0, users_in_room*number_of_rooms_in_apartment):
+                u = models.User.query.get((ai * number_of_rooms_in_apartment)+i+1)
+                c = models.Charge(bills=bills, rent=u.room.price, paid=choice([True, False]), rent_calculation_id=rc.id, user_id=u.id)
+                db.session.add(c)
+
+            db.session.commit()
 
